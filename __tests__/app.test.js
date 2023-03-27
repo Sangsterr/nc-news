c73bd2b440bf6f8eee8cc2d84c650d6601c9f760
Sangsterr <mrsangster@hotmail.com>
2023-03-27 16:42:10 +0100

    task 4 done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,16 +24,25 @@ describe('Returns 404 if submitting an invalid URL', () => {
 });
 
 describe('GET: /api/topics', () => {
-    it('should return all the topics table information when using a get request', () => {
+    it("200 responds with correct article", () => {
         return request(app)
-            .get('/api/topics')
+            .get("/api/articles/1")
             .expect(200)
             .then(({ body }) => {
-                expect(body).toHaveLength(3)
-                body.forEach((topic) => {
-                    expect(topic).toHaveProperty("description", expect.any(String));
-                    expect(topic).toHaveProperty("slug", expect.any(String));
-                })
+                expect(body[0]).toEqual({
+
+                    article_id: 1,
+                    title: 'Living in the shadow of a great man',
+                    topic: 'mitch',
+                    author: 'butter_bridge',
+                    body: 'I find this existence challenging',
+                    created_at: '2020-07-09T20:11:00.000Z',
+                    votes: 100,
+                    article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+
+                }
+                )
+                expect(body).toBeInstanceOf(Object)
             })
     });
 
